8248a1d3ceff0001614e4f38d17fca942f2a16ce
vfranco00 <victorfranco02@outlook.com>
2021-12-23 01:17:25 -0300

    Exercicios 4.1

---
+++ Fundamentos/Bloco_04-Java_Script_E_Logica_De_Programação/primeiros_passos.js
@@ -0,0 +1,15 @@
+let a = 5;
+let b = 7;
+
+let soma = (a + b); 
+let sub = (a - b);
+let mult = (a * b);
+let div = (a / b);
+let mod = (a % b);
+
+console.log('Resultados:'+
+'\n Soma: ' + soma +
+'\n Subtração: ' + sub +
+'\n Multiplicação: ' + mult +
+'\n Divisão: ' + div +
+'\n Módulo: ' + mod);
